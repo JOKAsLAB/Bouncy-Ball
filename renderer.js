f86edf5db66fb7fb141f168d27f0daed49abc4dd
JocaMartins33 <jokaslab233@gmail.com>
2025-05-13 14:46:36 +0100

    update

--- renderer.js
+++ renderer.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 
-const renderer = new THREE.WebGLRenderer({ antialias: true }); // Adicione antialias: true
+const renderer = new THREE.WebGLRenderer({ antialias: false }); // Adicione antialias: true
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap; // Ou outro tipo de sombra
